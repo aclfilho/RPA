3ed8852fb1c9cb6a72c41dbed053e0fc42c63ff2
Andre <coronel.filho@hotmail.com>
2025-02-03 15:15:00 -0300

    Melhorias com WebDriverWait

--- RPA.py
+++ RPA.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import pandas as pd
 import time
 
@@ -12,22 +14,27 @@ driver = webdriver.Edge()
 url = "https://www.bcb.gov.br/"
 driver.get(url)
 
-#aguardando abrir
-time.sleep(5)
+try:
+    
+    #encontrando cotação do dolar
+    cotacao_dolar = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="home"]/div/div[1]/div[1]/div/cotacao/table[1]/tbody/tr[2]/td[2]/span'))).text
 
-#encontrando cotação do dolar
-cotacao_dolar = driver.find_element(By.XPATH, '//*[@id="home"]/div/div[1]/div[1]/div/cotacao/table[1]/tbody/tr[2]/td[3]').text
+    #encontrando cotação do euro
+    cotacao_euro = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="home"]/div/div[1]/div[1]/div/cotacao/table[2]/tbody/tr[2]/td[2]/span'))).text
 
-#encontrando cotação do euro
-cotacao_euro = driver.find_element(By.XPATH, '//*[@id="home"]/div/div[1]/div[1]/div/cotacao/table[2]/tbody/tr[2]/td[3]/span').text
+    #criar um dataframe
+    dados = pd.DataFrame({"Moeda":["Dólar", "Euro"], "Cotação": [cotacao_dolar, cotacao_euro]})
 
-#fechar o navegador
-driver.quit()
+    # salvando em execel(xlsx)
+    dados.to_excel("cotacoes.xlsx", index=False)
 
-#criar um dataframe
-dados = pd.DataFrame({"Moeda":["Dólar", "Euro"], "Cotação": [cotacao_dolar, cotacao_euro]})
+    print("Relatório gerado!")
+    
+except Exception as e:
+    print(f"Erro ao buscar cotações: {e}")
+    
+finally:
+    driver.quit()
 
-# salvando em execel(xlsx)
-dados.to_excel("cotacoes.xlsx", index=False)
 
-print("Relatório gerado!")
+    
