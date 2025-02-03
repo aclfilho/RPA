c6697508d5eba8517ecbf2886e731bfda310fc84
Andre <coronel.filho@hotmail.com>
2025-02-03 15:35:04 -0300

    Melhoriaas na cotacao

--- RPA.py
+++ RPA.py
@@ -14,18 +14,29 @@ driver = webdriver.Edge()
 url = "https://www.bcb.gov.br/"
 driver.get(url)
 
+XPATH_DOLAR = '//*[@id="home"]/div/div[1]/div[1]/div/cotacao/table[1]/tbody/tr[2]/td[2]/span'
+XPATH_EURO = '//*[@id="home"]/div/div[1]/div[1]/div/cotacao/table[2]/tbody/tr[2]/td[2]/span'
+
+# def para formatar a cotação para float
+def formatar_cotacao(valor):
+    return float(valor.replace(",", "."))
+
 try:
-    
     #encontrando cotação do dolar
-    cotacao_dolar = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="home"]/div/div[1]/div[1]/div/cotacao/table[1]/tbody/tr[2]/td[2]/span'))).text
+    cotacao_dolar = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.XPATH, XPATH_DOLAR))).text
 
     #encontrando cotação do euro
-    cotacao_euro = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="home"]/div/div[1]/div[1]/div/cotacao/table[2]/tbody/tr[2]/td[2]/span'))).text
+    cotacao_euro = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.XPATH, XPATH_EURO))).text
 
     #criar um dataframe
-    dados = pd.DataFrame({"Moeda":["Dólar", "Euro"], "Cotação": [cotacao_dolar, cotacao_euro]})
+    dados = pd.DataFrame({
+        "Moeda": ["Dólar", "Euro"],
+        "Cotação": [formatar_cotacao(cotacao_dolar), formatar_cotacao(cotacao_euro)]        
+    })
 
-    # salvando em execel(xlsx)
+    #salvando em execel(xlsx)
     dados.to_excel("cotacoes.xlsx", index=False)
 
     print("Relatório gerado!")
@@ -34,7 +45,8 @@ except Exception as e:
     print(f"Erro ao buscar cotações: {e}")
     
 finally:
-    driver.quit()
+    if 'driver' in locals():
+        driver.quit
 
 
     
